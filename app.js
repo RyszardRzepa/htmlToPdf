b8e93abedafd0876f2cb64251061b13a5d0a26cf
ryszard <ryszrzepa@gmail.com>
2023-03-05 19:53:14 +0100

    w

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const puppeteer require('puppeteer')
+const puppeteer = require('puppeteer')
 const hb = require('handlebars')
 
 app.get("/", (req, res) => {
